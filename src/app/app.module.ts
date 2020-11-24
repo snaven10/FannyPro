c096e166649bdb6765fa613de3355ad6d6b69fce
snaven10 <frjsamuel@gmail.com>
2020-11-23 22:52:36 -0600

    creating product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './components/app/app.component';
 import { NavigationsModule } from './components/_module/navigations/navigations.module';
 import { CategorieModule } from './components/_module/categorie/categorie.module';
 import { SubcategorieModule } from './components/_module/subcategorie/subcategorie.module';
+import { DetailComponent } from './components/pages/products/productdetail/detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DetailComponent,
   ],
   imports: [
     NavigationsModule,
