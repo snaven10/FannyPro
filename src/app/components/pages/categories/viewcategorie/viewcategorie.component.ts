db509a2ac109e2f2bf58d655627294dd78483482
snaven10 <frjsamuel@gmail.com>
2021-01-24 16:59:45 -0600

    Enabling functional selection

--- src/app/components/pages/categories/viewcategorie/viewcategorie.component.ts
+++ src/app/components/pages/categories/viewcategorie/viewcategorie.component.ts
@@ -3,6 +3,7 @@ import { categorie } from 'src/app/components/_model/categorie.model';
 import { CategorieService } from 'src/app/components/_service/categorie/categorie.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-viewcategorie',
@@ -12,12 +13,37 @@ import { MatTableDataSource } from '@angular/material/table';
 export class ViewcategorieComponent implements OnInit, AfterViewInit {
   dataSource: categorie[];
 
-  displayedColumns: string[] = ['check','id','name'];
+  displayedColumns: string[] = ['select','id','name'];
 
   dataSources = new MatTableDataSource<categorie>(this.CategorieService.getCategorie());
+  selection = new SelectionModel<categorie>(true,[]);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  /** Si el número de elementos seleccionados coincide con el número total de filas. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSources.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selecciona todas las filas si no están todas seleccionadas; de lo contrario, selección clara. */
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSources.data.forEach(row => this.selection.select(row));
+  }
+
+  /** La etiqueta de la casilla de verificación en la fila pasada */
+  checkboxLabel(row?: categorie): string {
+    if (!row) {
+      return `${this.isAllSelected() ? "select" : "deselect"} all`;
+    }
+    return `${
+      this.selection.isSelected(row) ? "deselect" : "select"
+    } row ${row.id + 1}`;
+  }
+  
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSources.filter = filterValue.trim().toLowerCase();
--- src/app/components/pages/products/viewproduct/viewproduct.component.ts
+++ src/app/components/pages/products/viewproduct/viewproduct.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { product } from './../../../_model/product.model';
 import { ProductService } from './../../../_service/product/product.service';
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-viewproduct',
@@ -11,10 +12,35 @@ import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 })
 export class ViewproductComponent implements OnInit, AfterViewInit {
   dataSource: product[];
-  displayedColumns: string[] = ['check','id','name','manofacture','count','location','description'];
+  displayedColumns: string[] = ['select','id','name','manofacture','count','location','description'];
   dataSources = new MatTableDataSource<product>(this.ProductService.getproduct());
+  selection = new SelectionModel<product>(true,[]);
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  
+
+  /** Si el número de elementos seleccionados coincide con el número total de filas. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSources.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selecciona todas las filas si no están todas seleccionadas; de lo contrario, selección clara. */
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSources.data.forEach(row => this.selection.select(row));
+  }
+
+  /** La etiqueta de la casilla de verificación en la fila pasada */
+  checkboxLabel(row?: product): string {
+    if (!row) {
+      return `${this.isAllSelected() ? "select" : "deselect"} all`;
+    }
+    return `${
+      this.selection.isSelected(row) ? "deselect" : "select"
+    } row ${row.id + 1}`;
+  }
   applyFilter(event:Event){
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSources.filter = filterValue.trim().toLocaleLowerCase();
